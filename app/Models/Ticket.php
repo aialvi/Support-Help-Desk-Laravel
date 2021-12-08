700907b6c21a7eac4e43dae73a3ca0f10ede2118
Aminul Islam Alvi <aminulialvi@gmail.com>
2021-12-08 09:38:41 +0600

    Modify ticket and category model

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Ticket extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id', 'category_id', 'ticket_id', 'title', 'priority', 'message', 'status',
+    ];
 }
